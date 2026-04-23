7009a75e507837fc19a8ea89a23cf9c2e61639e2
alex blade <aalexblade@gmail.com>
2026-04-23 18:26:34 +0300

    add btn

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,21 @@ function App() {
         <div className=" border-sky-500 bg-amber-500 border-r-4">three</div>
         <div className="bg-volet-200 border-purple-800 border-l-8">four</div>
       </div>
+
+      <div>
+        <button className="border-2 border-green-600 text-green-400 px-8 py-2 m-4 rouded-md">
+          first
+        </button>
+        <button className="rounded-lg bg-sky-500 text-white px-10 py-4 text-3xl hover:m-4 cursor-pointer">
+          second
+        </button>
+        <button className="bg-violet-100 text-violet-600 px-8 py-4 rounded-full m-4">
+          third
+        </button>
+        <button className="bg-cyan-700 m-4 text-cyan-200 p-10 rounded-lg hover:bg-cyan-200 hover:text-cyan-700 cursor-pointer">
+          four
+        </button>
+      </div>
     </>
   );
 }
