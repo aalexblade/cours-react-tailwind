a3c02ea32901f2325d274b918c9586a1ce7ded77
alex blade <aalexblade@gmail.com>
2026-04-23 17:52:01 +0300

    add color and bg to tex

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello Tailwind!
-    </h1>
-  )
-}
+    <>
+      <h1 className="text-2xl font-bold underline text-black-300">
+        Hello Tailwind!
+      </h1>
+      <div className="text-rosw-300 bg-red-700">one</div>
+      <div className="text-slate-400 border-2 border-slate-500">two</div>
 
+      <div className="text-xl text-purple-500">
+        <div className=" border-sky-500 bg-amber-500 border-r-4">three</div>
+        <div className="bg-volet-200 border-purple-800 border-l-8">four</div>
+      </div>
+    </>
+  );
+}
 
 export default App;
