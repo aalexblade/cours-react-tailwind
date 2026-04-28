9e6fa2c5600302ea5df747677036fdae1ba502c2
alex blade <aalexblade@gmail.com>
2026-04-28 22:16:34 +0300

    add component header

--- search_page_design/src/components/Header.jsx
+++ search_page_design/src/components/Header.jsx
@@ -1,6 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+
 const Header = () => {
   return (
-  <div>Helow Taiwind</div>
+    <div className="flex justify-between items-center px-8 py-4 bg-orange-200 text-xl text-orange-700">
+      <div className="flex items-center">
+        <div className="mx-4">About</div>
+        <div className="mx-4">Store</div>
+      </div>
+      <div className="flex items-center">
+        <div className="mx-4">Preferences</div>
+       <FontAwesomeIcon icon={faUser} className="text-2xl" />
+      </div>
+    </div>
   );
 };
 
